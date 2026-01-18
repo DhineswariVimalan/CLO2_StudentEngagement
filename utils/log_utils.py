e236448851c48f0e2365169c46592467d6d1d4ae
Dhineswari Vimalan <vkdhineswari1507@gmail.com>
2026-01-18 20:37:10 +0800

    Added Streamlit prediction app with feedback logging

--- utils/log_utils.py
+++ utils/log_utils.py
@@ -2,44 +2,18 @@ import csv
 import os
 from datetime import datetime
 
-LOG_FILE = "data/monitoring_logs.csv"
+LOG_FILE = "monitoring_logs.csv"
 
-def log_prediction(
-    total_clicks,
-    active_days,
-    interactive_clicks,
-    content_clicks,
-    prediction_v1,
-    prediction_v2,
-    latency_ms,
-    feedback
-):
+def log_prediction(model_version, prediction, latency):
     file_exists = os.path.isfile(LOG_FILE)
 
     with open(LOG_FILE, mode="a", newline="") as file:
         writer = csv.writer(file)
-
         if not file_exists:
-            writer.writerow([
-                "timestamp",
-                "total_clicks",
-                "active_days",
-                "interactive_clicks",
-                "content_clicks",
-                "prediction_v1",
-                "prediction_v2",
-                "latency_ms",
-                "feedback"
-            ])
-
+            writer.writerow(["timestamp", "model_version", "prediction", "latency"])
         writer.writerow([
-            datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
-            total_clicks,
-            active_days,
-            interactive_clicks,
-            content_clicks,
-            prediction_v1,
-            prediction_v2,
-            latency_ms,
-            feedback
+            datetime.now(),
+            model_version,
+            prediction,
+            latency
         ])
